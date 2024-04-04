cab562afad37372772012098a25f3112502f8b55
jwoon0606 <jwoon0606@gmail.com>
2024-04-04 15:23:43 +0900

    Create cstack.cpp

---
+++ DataStructure/study/cstack.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define max 25
+
+char name[max];
+int top = -1;
+
+void push(char item){
+    name[++top] = item;
+}
+
+char pop(){
+    return name[top--];
+}
+
+int main(){
+    push('H');
+    push('a');
+    push('n');
+    printf("%c",pop());
+    printf("%c",pop());
+    printf("%c\n",pop());
+
+    return 0;
+}
