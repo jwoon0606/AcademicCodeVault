36fba928e64c827d9c1750a34f5fe42997683ff1
jwoon0606 <jwoon0606@gmail.com>
2024-04-07 23:58:25 +0900

    Update add.c

--- OperatingSystem/add.c
+++ OperatingSystem/add.c
@@ -3,9 +3,11 @@
 int main(){
     int a = 0, b = 0;
 
+    // Get two integers from user
     printf("Input two integers: ");
     scanf("%d %d",&a,&b);
 
+    // display the result
     printf("%d + %d = %d\n",a,b,a+b);
     return 0;   
 }
