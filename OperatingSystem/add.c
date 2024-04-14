d727926ec66230bf5ffadb5afcf932d5053468d6
jwoon0606 <jwoon0606@gmail.com>
2024-04-14 18:12:58 +0900

    Update files

--- OperatingSystem/add.c
+++ OperatingSystem/add.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int a = 0, b = 0;
 
     // Get two integers from user
     printf("Input two integers: ");
-    scanf("%d %d",&a,&b);
+    scanf("%d %d", &a, &b);
 
     // display the result
-    printf("%d + %d = %d\n",a,b,a+b);
-    return 0;   
+    printf("%d + %d = %d\n", a, b, a + b);
+    return 0;
 }
--- OperatingSystem/startend.c
+++ OperatingSystem/startend.c
@@ -5,40 +5,45 @@
 #define MAX_LEN 256
 #define WORD_LEN 50
 
-typedef struct _WORD{
+typedef struct _WORD
+{
 	int start;
 	int end;
 	char word[WORD_LEN];
-}WORD;
+} WORD;
 
 int main()
 {
-	WORD w[MAX_LEN] = {0,0,""};
-	char text [MAX_LEN] = "";
+	WORD w[MAX_LEN] = {0, 0, ""};
+	char text[MAX_LEN] = "";
 
 	// read a text from the user
 	printf("input a text: ");
-	fgets(text,MAX_LEN,stdin);
-	text[strlen(text)-1] = 0;
+	fgets(text, MAX_LEN, stdin);
+	text[strlen(text) - 1] = 0;
 
 	// fill space with blanks
 	printf("              ");
 
 	// display mark to check start and end
-	int pre_char = 0;	// previous character(0: whitespace, 1: not whitespace)
+	int pre_char = 0; // previous character(0: whitespace, 1: not whitespace)
 	int word_count = 0;
 	int text_count = 0;
 	int i = 0;
-	for(i=0; i<strlen(text); i++){
+	for (i = 0; i < strlen(text); i++)
+	{
 		// case 1: current text is whitespace
-		if(isspace(text[i])){
+		if (isspace(text[i]))
+		{
 			// case 1-1: previous character is whitespace
-			if(pre_char == 0){		//previous character is whitespace	
+			if (pre_char == 0)
+			{ // previous character is whitespace
 				printf(" ");
 			}
-			
+
 			// case 1-2: previous character is not whitespace (end point)
-			else if(pre_char == 1){
+			else if (pre_char == 1)
+			{
 				printf("]");
 				w[word_count].end = i;
 				word_count++;
@@ -47,16 +52,19 @@ int main()
 			pre_char = 0;
 		}
 		// case 2: current text is not whitespace
-		else{
+		else
+		{
 			// case 2-1: previous character is whitespace (strat point)
-			if(pre_char == 0){
+			if (pre_char == 0)
+			{
 				printf("[");
 				w[word_count].start = i;
 				w[word_count].word[text_count] = text[i];
 				text_count++;
 			}
 			// case 2-2: previous character is not whitespace
-			else if(pre_char == 1){
+			else if (pre_char == 1)
+			{
 				printf(" ");
 				w[word_count].word[text_count] = text[i];
 				text_count++;
@@ -64,7 +72,8 @@ int main()
 			pre_char = 1;
 		}
 	}
-	if(pre_char == 1){
+	if (pre_char == 1)
+	{
 		printf("]");
 		w[word_count].end = i;
 		word_count++;
@@ -72,8 +81,9 @@ int main()
 	printf("\n\n");
 
 	// display the word[i]
-	for(int i=0; i<word_count; i++){
-		printf("words[%d] = (%d,%d,%s)\n",i,w[i].start,w[i].end,w[i].word);
+	for (int i = 0; i < word_count; i++)
+	{
+		printf("words[%d] = (%d,%d,%s)\n", i, w[i].start, w[i].end, w[i].word);
 	}
 
 	return 0;
