2805ae3bcecf5988998eb6ee136e804240d2cc94
jwoon0606 <jwoon0606@gmail.com>
2024-04-18 16:13:10 +0900

    Create DS week7 files

---
+++ DataStructure/week7/list2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+typedef struct t_list_node {
+    string data;
+    struct t_list_node *link;
+} list_node;
+
+int main(){
+    list_node *head =new list_node;
+    head->data = "cat";
+
+    head->link = new list_node;
+    head->link->data = "bat";
+    head->link->link = NULL;
+    // cout << head->link->data << endl;    // for test
+
+    list_node *node = new list_node;
+    node->data = "vat";
+    node->link = head->link;
+    head->link = node;
+
+    cout << "head->link->data: " <<head->link->data << endl;   // "vat"
+    cout << "head->link->link->data: " << head->link->link->data << endl; // "bat"
+}
---
+++ DataStructure/week7/list3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+typedef struct t_list_node {
+    int data;
+    struct t_list_node *link;
+} list_node;
+
+int main(){
+    list_node *head =new list_node;
+    head->data = 1;
+    head->link = NULL;
+    list_node *cur;
+    cur = head;
+
+    for(int i=2; i<=10; i++){
+        cur->link = new list_node;
+        cur->link->data = i;
+        cur->link->link = NULL;
+        cur = cur->link;
+    }
+
+    cur = head;
+    while(cur){
+        cout << cur->data << '\t';
+        cur = cur->link;
+    }
+    cout << endl;
+
+    cur = head;
+    list_node *temp;
+    while(cur){
+        temp = cur->link;
+        cout << "delete " << cur->data << endl;
+        delete cur;
+        cur = temp;
+    }
+    head = NULL;
+
+    if(head == NULL){
+        cout << "head is NULL" << endl;
+    }
+
+    return 0;
+}
