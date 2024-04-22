80cbc279aaeaed086f4e00cf165138503bb60e11
jwoon0606 <jwoon0606@gmail.com>
2024-04-22 15:33:28 +0900

    Update Speaker

--- DataStructure/Speaker.cpp
+++ DataStructure/Speaker.cpp
@@ -9,35 +9,46 @@ Reference
 using namespace std;
 
 // default constructor
-Speaker::Speaker():OnOff(false),volume(1){}
+Speaker::Speaker() : OnOff(false), volume(1) {}
 
 // changes on/off state
-void Speaker::PowerOnOff(){
+void Speaker::PowerOnOff()
+{
     OnOff = (OnOff == true) ? false : true;
     volume = (OnOff == true) ? 1 : 0;
 }
 
 // increases volume by 1 level(maximum level is level 5)
-void Speaker::VolumeUp(){
-    if(volume == 5) printf("Volume is arleady Max level 5\n");
-    else volume += 1;
+void Speaker::VolumeUp()
+{
+    if (volume == 5)
+        printf("Volume is arleady Max level 5\n");
+    else
+        volume += 1;
 }
 
 // decrease volume down by 1 level
-void Speaker::VolumeDown(){
-    if(volume == 1) printf("Volume is arleady MIN level 1\n");
-    else volume -= 1;
+void Speaker::VolumeDown()
+{
+    if (volume == 1)
+        printf("Volume is arleady MIN level 1\n");
+    else
+        volume -= 1;
 }
 
 // shows if speaker is on or off
-bool Speaker::IsOn(){
+bool Speaker::IsOn()
+{
     return OnOff;
 }
 
 // speaker's status
-void Speaker::ShowStatus(){
-    if(OnOff == true) printf("Current volume level is %d.\n",volume);
-    else printf("Speaker is off now.\n");
+void Speaker::ShowStatus()
+{
+    if (OnOff == true)
+        printf("Current volume level is %d.\n", volume);
+    else
+        printf("Speaker is off now.\n");
 }
 
 // descructor
--- DataStructure/Speaker.h
+++ DataStructure/Speaker.h
@@ -9,20 +9,20 @@ Reference
 
 class Speaker
 {
-    private:
-        bool OnOff; // on : true, off : false
-        int volume; // level 1 ~ 5
+private:
+    bool OnOff; // on : true, off : false
+    int volume; // level 1 ~ 5
 
-    public:
-        Speaker(); // default constructor
-        
-        void PowerOnOff(); // changes on/off state
-        void VolumeUp(); // increases volume by 1 level(maximum level is level 5)
-        void VolumeDown(); // decrease volume down by 1 level
-        bool IsOn(); // shows if speaker is on or off
-        void ShowStatus(); // speaker's status
-        
-        ~Speaker(); // descructor
+public:
+    Speaker(); // default constructor
+
+    void PowerOnOff(); // changes on/off state
+    void VolumeUp();   // increases volume by 1 level(maximum level is level 5)
+    void VolumeDown(); // decrease volume down by 1 level
+    bool IsOn();       // shows if speaker is on or off
+    void ShowStatus(); // speaker's status
+
+    ~Speaker(); // descructor
 };
 
 #endif
