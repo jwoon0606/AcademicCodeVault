493a3af503526afed60ce406c03fc8f400e4d07c
jwoon0606 <jwoon0606@gmail.com>
2024-05-08 15:12:46 +0900

    Update

--- DataStructure/TIME/time.cpp
+++ DataStructure/TIME/time.cpp
@@ -2,11 +2,11 @@
 #include "time.h"
 #include <iostream>
 // Default Constructor
-Time::Time() : hrs(0), mins(0), secs(0){}
+Time::Time() : hrs(0), mins(0), secs(0) {}
 // parametrized Constructor
-Time::Time(int hours, int minutes, int seconds) : hrs(hours), mins(minutes), secs(seconds){}
+Time::Time(int hours, int minutes, int seconds) : hrs(hours), mins(minutes), secs(seconds) {}
 // Destructor
-Time::~Time(){}
+Time::~Time() {}
 void Time::Set(int hours, int minutes, int seconds)
 {
     hrs = hours;
