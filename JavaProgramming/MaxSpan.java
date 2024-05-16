2b06e48bded73f678c25060f79b6132ba7b5c829
jwoon0606 <jwoon0606@gmail.com>
2024-05-16 10:43:26 +0900

    Update MaxSpan.java

--- JavaProgramming/MaxSpan.java
+++ JavaProgramming/MaxSpan.java
@@ -1,26 +1,27 @@
 public class MaxSpan {
   public static void main(String[] args) {
-      int[] arr1 = {1, 2, 1, 1, 3};
-      int[] arr2 = {1, 4, 2, 1, 4, 1, 4};
-      int[] arr3 = {1, 4, 2, 1, 4, 4, 4};
-      
-      System.out.println(maxSpan(arr1)); // Output: 4
-      System.out.println(maxSpan(arr2)); // Output: 6
-      System.out.println(maxSpan(arr3)); // Output: 6
+    int[] arr1 = { 1, 2, 1, 1, 3 };
+    int[] arr2 = { 1, 4, 2, 1, 4, 1, 4 };
+    int[] arr3 = { 1, 4, 2, 1, 4, 4, 4 };
+
+    System.out.println(maxSpan(arr1)); // Output: 4
+    System.out.println(maxSpan(arr2)); // Output: 6
+    System.out.println(maxSpan(arr3)); // Output: 6
   }
 
   public static int maxSpan(int[] nums) {
     int max_span = 0;
-    for(int i=0; i<nums.length; i++){
-      int j=0;
-      for(j=nums.length -1; j>=i; j--){
-        if(nums[i] == nums[j]) break;
+    for (int i = 0; i < nums.length; i++) {
+      int j = 0;
+      for (j = nums.length - 1; j >= i; j--) {
+        if (nums[i] == nums[j])
+          break;
       }
       int span = j - i + 1;
-      if(span > max_span) max_span = span;
+      if (span > max_span)
+        max_span = span;
     }
-    
+
     return max_span;
   }
 }
-  
