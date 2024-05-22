1bd46c7852c218d10489eb509e1204101122675b
jwoon0606 <jwoon0606@gmail.com>
2024-05-22 19:52:52 +0900

    Update

--- DataStructure/hw1/hw1_2.cpp
+++ DataStructure/hw1/hw1_2.cpp
@@ -4,75 +4,85 @@
 #include <iostream>
 using namespace std;
 
-class RC 
+class RC
 {
-    private:
+private:
     string dorm;
     int room;
 
-    public:
+public:
     // default constructor
     RC() : dorm(" "), room(0) {}
     // parameterized constructor
-    RC(string name, int num){
+    RC(string name, int num)
+    {
         dorm = name;
         room = num;
     }
     // get dorm name
-    string getDorm() const {
+    string getDorm() const
+    {
         return dorm;
     }
     // assign dorm
-    void AssignDorm(string d){
+    void AssignDorm(string d)
+    {
         dorm = d;
     }
     // get room number
-    int getRoom() const {
+    int getRoom() const
+    {
         return room;
     }
     // assign room
-    void AssignRoom(int r){
+    void AssignRoom(int r)
+    {
         room = r;
     }
 };
 
 class Student
 {
-    private:
+private:
     string name;
     RC address;
 
-    public:
+public:
     // constructor
-    Student(string name, RC &addr){
+    Student(string name, RC &addr)
+    {
         this->name = name;
         address = addr;
     }
     // get student name
-    string getName() const {
+    string getName() const
+    {
         return name;
     }
     // get address (RC name & room number)
     // of student
-    RC getAddress() const {
+    RC getAddress() const
+    {
         return address;
     }
     // display student name and address
     // using member function of class Student
-    void displayInfo() const {
+    void displayInfo() const
+    {
         cout << "Student Name: " << name << endl;
-        cout << "Address is " << address.getDorm()<< ", " << address.getRoom() << endl;
+        cout << "Address is " << address.getDorm() << ", " << address.getRoom() << endl;
     }
 };
 
-int main(){
+int main()
+{
     RC kuyper1;
     RC kuyper2("Creation", 204);
     RC torrey1("Vision", 312);
     // Create a Customer object using the Address object
     Student student1("Alan Turing", kuyper1);
     Student student2("Beatles", torrey1);
-    Student student3("Timothee Chalamet",kuyper2);
+    Student student3("Timothee Chalamet", kuyper2);
     // Display customer information
     student1.displayInfo();
     student2.displayInfo();
