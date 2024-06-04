4566f4d67a85072ea5a2399b5055bec7056c84b2
jwoon0606 <jwoon0606@gmail.com>
2024-06-04 21:24:22 +0900

    Update

--- JavaProgramming/InforHideCircle.java
+++ JavaProgramming/InforHideCircle.java
@@ -1,33 +1,36 @@
-class Circle{
-    private double rad = 0;
-    final double PI = 3.14;
+class Circle {
+  private double rad = 0;
+  final double PI = 3.14;
 
-  public Circle(double r){
+  public Circle(double r) {
     setRad(r);
   }
-  public void setRad(double r){
-    if(r < 0){
+
+  public void setRad(double r) {
+    if (r < 0) {
       rad = 0;
       return;
     }
     rad = r;
   }
-  public double getRad(){
+
+  public double getRad() {
     return rad;
   }
-  public double getArea(){
+
+  public double getArea() {
     return (rad * rad) * PI;
   }
 }
 
-class InfoHideCircle{
-  public static void main(String args[]){
+class InfoHideCircle {
+  public static void main(String args[]) {
     Circle c = new Circle(1.5);
     System.out.println("반지름 : " + c.getRad());
     System.out.println("넓 이 : " + c.getArea() + "\n");
 
-	c.setRad(3.4);
-	System.out.println("반지름 : " + c.getRad());
-	System.out.println("넓 이 : " + c.getArea());
+    c.setRad(3.4);
+    System.out.println("반지름 : " + c.getRad());
+    System.out.println("넓 이 : " + c.getArea());
   }
 }
